028ddc459fc3219ba9b55cfdaab007953f1379b4
yafit amare <57882024+YAFETAMARE@users.noreply.github.com>
2021-09-28 23:09:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+var questions = [
+    {
+      title: "An HTML document can contain _____?",
+      choices: ["Attributes", "Tags", "Raw text", "All the answers are true"],
+      answer: "All the answers are true"
+    },
 
+    {
+      title: "A page designed in HTML is called _____",
+      choices: ["Web Page", "Application", "Front-end", "Cover page"],
+      answer: "Web Page"
+    },
+
+    {
+      title: "An HTML document is saved with the ____ extension.",
+      choices: [".html", ".htl", ".hml", ".htnl"],
+      answer: ".html"
+    }, 
+
+    {
+      title: "The HTML document contains a root tag called ____",
+      choices: ["HEAD", " HTML", "Body", "Title"],
+      answer: "HTML"
+    }, 
+    {
+      title: "If we want to place text around an image, which CSS property should we use?",
+      choices: ["push", "float", "align", "wrap"],
+      answer: "float"
+    }, 
+   
+  ];
